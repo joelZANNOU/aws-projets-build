2fa335ee365e7cc6ac1db30efb968be86660fb94
ZANNOU Jesugnon Joel <Jesugnonjoel.zannou@edu.univ-paris13.fr>
2026-01-09 20:00:01 +0100

    feature : implémentation de la feat Hello

---
+++ test_app.py
@@ -0,0 +1,9 @@
+import unittest
+from app import say_hello
+class TestApp(unittest.TestCase):
+    def test_say_hello(self):
+        self.assertEqual(say_hello("AWS"), "Hello, AWS!")
+        self.assertEqual(say_hello("Bob"), "Hello, Bob!")
+if __name__ == '__main__':
+    unittest.main()
+    
