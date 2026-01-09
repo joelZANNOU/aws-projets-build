9cc9dc2e3da82861260493bed0ff264230d40b66
ZANNOU Jesugnon Joel <Jesugnonjoel.zannou@edu.univ-paris13.fr>
2026-01-09 21:19:21 +0100

    correction

--- test_app.py
+++ test_app.py
@@ -1,9 +1,11 @@
 import unittest
+
 from app import say_hello
+
+
 class TestApp(unittest.TestCase):
     def test_say_hello(self):
         self.assertEqual(say_hello("AWS"), "Hello, AWS!")
         self.assertEqual(say_hello("Bob"), "Hello, Bob!")
 if __name__ == '__main__':
     unittest.main()
-    
